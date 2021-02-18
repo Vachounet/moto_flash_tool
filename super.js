0c6ada897a98487f9563fb3b109fc36e94f602bc
Vachounet <vachounet@live.fr>
2021-02-18 14:14:53 +0100

    super: don't crash if product_b/vendor_b don't exist

--- super.js
+++ super.js
@@ -48,41 +48,72 @@ async function flashSuperBPartitions(firmwareFolder) {
     }
   }
 
-  await fastbootExec({ cmd: "delete-logical-partition product_b" });
-  await fastbootExec({ cmd: "delete-logical-partition vendor_b" });
+  try {
+    await fastbootExec({ cmd: "delete-logical-partition product_b" });
+  } catch (e) {
+    // ignore, partition doesn't exists
+  }
 
-  let partitionSize = await fastbootExec({
-    cmd: "getvar partition-size:vendor_a",
-  });
-  partitionSize = partitionSize.split(":")[2].trim().split("\n")[0];
-  await fastbootExec({
-    cmd: "create-logical-partition vendor_b " + partitionSize,
-  });
+  try {
+    await fastbootExec({ cmd: "delete-logical-partition vendor_b" });
+  } catch (e) {
+    // ignore, partition doesn't exists
+  }
 
-  partitionSize = await fastbootExec({
-    cmd: "getvar partition-size:product_a",
-  });
-  partitionSize = partitionSize.split(":")[2].trim().split("\n")[0];
-  await fastbootExec({
-    cmd: "create-logical-partition product_b " + partitionSize,
-  });
+  let partitionSize = 0x0;
+  try {
+    let partitionSize = await fastbootExec({
+      cmd: "getvar partition-size:vendor_a",
+    });
+    partitionSize = partitionSize.split(":")[2].trim().split("\n")[0];
+    await fastbootExec({
+      cmd: "create-logical-partition vendor_b " + partitionSize,
+    });
+  } catch (e) {
+    console.log("\nUnable to create vendor_b");
+  }
+
+  try {
+    partitionSize = await fastbootExec({
+      cmd: "getvar partition-size:product_a",
+    });
+    partitionSize = partitionSize.split(":")[2].trim().split("\n")[0];
+    await fastbootExec({
+      cmd: "create-logical-partition product_b " + partitionSize,
+    });
+  } catch (e) {
+    console.log("\nUnable to create product_b");
+  }
 
   console.log("\nFlashing system/product/vendor to slot b");
 
-  let result = await fastbootExec({
-    cmd: "flash product_b " + firmwareFolder + "/product_a.img",
-  });
-  console.log(result)
+  let result = "";
+  try {
+    result = await fastbootExec({
+      cmd: "flash product_b " + firmwareFolder + "/product_a.img",
+    });
+    console.log(result);
+  } catch (e) {
+    console.log("\nUnable to flash product_b");
+  }
 
-  result = await fastbootExec({
-    cmd: "flash vendor_b " + firmwareFolder + "/vendor_a.img",
-  });
-  console.log(result)
+  try {
+    result = await fastbootExec({
+      cmd: "flash vendor_b " + firmwareFolder + "/vendor_a.img",
+    });
+    console.log(result);
+  } catch (e) {
+    console.log("\nUnable to flash vendor_b");
+  }
 
-  result = await fastbootExec({
-    cmd: "flash system_b " + firmwareFolder + "/system_a.img",
-  });
-  console.log(result)
+  try {
+    result = await fastbootExec({
+      cmd: "flash system_b " + firmwareFolder + "/system_a.img",
+    });
+    console.log(result);
+  } catch (e) {
+    console.log("\nUnable to flash system_b");
+  }
 
   console.log("Done");
 }
